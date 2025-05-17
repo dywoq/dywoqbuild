2cab0032b0a67a29acd449a48cc63135dc3e06e4
dywoq <aleks.koyf@gmail.com>
2025-05-17 18:04:22 +0300

    [dywoqbuild_lib] fix documentation of ErrWrongStandard, changing values

--- dywoqbuild_lib/errors.go
+++ dywoqbuild_lib/errors.go
@@ -3,7 +3,7 @@ package dywoqbuildlib
 import "errors"
 
 // ErrWrongStandard is a error indicating that the chosen standard by a user may be wrong.
-var ErrWrongStandard = errors.New("standard c++ is not correct, values: c++11, c++14, c++17, c++20, c++23, c++2a")
+var ErrWrongStandard = errors.New("standard c++ is not correct, values: c++98, c++03, c++11, c++14, c++17, c++20, c++23, c++2a")
 
 // ErrWrongSuffix is a error that means the one of source files may have wrong suffix.
 var ErrWrongSuffix = errors.New("suffix of one source files is not correct, allowed suffixes: .C, .cc, .cpp, .cxx, c++, .h, .H, .hh, .hpp, .hxx, h++")
